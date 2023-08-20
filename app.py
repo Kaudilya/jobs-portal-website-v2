b80371cd8741b9e0afad76e3fb754b9be252b2ac
Kaudilya <137365036+Kaudilya@users.noreply.github.com>
2023-08-20 04:40:10 +0000

    Added connection to Database

--- app.py
+++ app.py
@@ -1,36 +1,39 @@
 from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
 
 app = Flask(__name__)
 
-JOBS = [
-  {
-    'id': 1,
-    'title': 'Software Engineer',
-    'location': 'USA',
-    'salary': '$150,000',
-  },
-  {
-    'id': 2,
-    'title': 'Backend Engineer',
-    'location': 'USA',
-    'salary': '$250,000',
-  },
-  {
-    'id': 3,
-    'title': 'Frontend Engineer',
-    'location': 'USA',
-  },
-]
+# JOBS = [
+#   {
+#     'id': 1,
+#     'title': 'Software Engineer',
+#     'location': 'USA',
+#     'salary': '$150,000',
+#   },
+#   {
+#     'id': 2,
+#     'title': 'Backend Engineer',
+#     'location': 'USA',
+#     'salary': '$250,000',
+#   },
+#   {
+#     'id': 3,
+#     'title': 'Frontend Engineer',
+#     'location': 'USA',
+#   },
+# ]
 
 
 @app.route("/")
 def hello():
-  return render_template('home.html', jobs=JOBS, company_name="Google")
+  jobs = load_jobs_from_db()
+  return render_template('home.html', jobs=jobs, company_name="Google")
 
 
-@app.route('/jobs')
+@app.route('/api/jobs')
 def list_jobs():
-  return jsonify(JOBS)
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
 
 
 if __name__ == "__main__":
---
+++ database.py
@@ -0,0 +1,34 @@
+# code to connect to database and extract data from database. - 2.0.20
+
+from sqlalchemy import create_engine, text
+import os
+
+db_connection_string = os.environ['DB_CONNECTION_STRING']
+
+engine = create_engine(db_connection_string,
+                       connect_args={"ssl": {
+                         "ssl_ca": "/etc/ssl/cert.pem",
+                       }})
+
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("Select * from jobs")
+    )  #any sql query, returns a list of values/tables. ; error is saying we need secure way to connect to DB.
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row._asdict()))
+
+  return jobs
+
+  # result_all = result.all()
+  # print("type(result_all) : ", type(result_all))
+  # print("result_all : ", result_all)
+  # print("type(result_all[0]) : ", type(result_all[0]))
+  # print("result_all[0] : ", result_all[0])
+  # # first_result_dict = result_all[0].__dict__
+  # first_result = result_all[0]
+  # first_result_dict = dict(first_result._asdict())
+  # print("first_result_dict",first_result_dict)
+  # print("type(first_result_dict)",type(first_result_dict))
